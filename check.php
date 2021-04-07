97902dd50ca25b0ed2eb17d00569f1d09408d47b
Dravichi Code <64124548+Dra-Co@users.noreply.github.com>
2021-04-07 13:32:59 +0700

    Update check.php

--- check.php
+++ check.php
@@ -3,5 +3,5 @@ $name = $_POST['data_name'];
 $json = file_get_contents('data.json');
 $datas = json_decode($json, true);
 $datas[$name]['complete'] = !$datas[$name]['complete'];
-file_put_contents('data.json', json_encode($datas), JSON_PRETTY_PRINT);
+file_put_contents('data.json', json_encode($datas));
 header("Location: index.php");
